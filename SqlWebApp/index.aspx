70626613d972efaad661fd686dcff7ab1af18fda
HSD112 <horvathstefandaniel@gmail.com>
2020-12-15 01:23:45 +0100

    Add project files.

---
+++ SqlWebApp/index.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="SqlWebApp.index" %>
+
+<!DOCTYPE html>
+<style>
+    body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+    }
+
+    html {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+    }
+
+    iframe {
+        height: 100%;
+        width: 100%;
+        margin: 0;
+        padding: 0;
+    }
+  </style>
+
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>SEP6 AIRPORT DATA</title>
+</head>
+<body>
+   
+    <iframe 
+        src="https://app.powerbi.com/reportEmbed?reportId=9b87e700-31e6-41e4-b837-8d7e26b3a1a8&autoAuth=true&ctid=97e1982b-375c-460e-9071-9067de86388b&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLW5vcnRoLWV1cm9wZS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D">
+    </iframe>
+
+</body>
+</html>
---
+++ SqlWebApp/index.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SqlWebApp
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            /*
+            //Connecting to db
+            string connString = "Server=tcp:sep6-server.database.windows.net,1433;Initial Catalog=AirData;Persist Security Info=False;User ID={};Password={};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            SqlConnection conn = new SqlConnection(connString);
+            conn.Open();
+
+            //Creating a command
+            SqlCommand cmd = new SqlCommand("SELECT * FROM [dbo].[airlines]");
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.Connection = conn;
+
+            //Read from db
+            string temp = "";
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())   //while reader still has data
+            {
+                temp += reader["carrier"].ToString();
+                temp += "&nbsp";
+                temp += reader["name"].ToString();
+                temp += "<br/>";
+            }
+
+            conn.Close();
+
+            //displaying data
+            lbl_test.Text = temp;
+            */
+
+        }
+    }
+}
